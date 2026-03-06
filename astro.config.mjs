5291757dc68253d427849063b80fe249ff8d0c8c
Cork0158 <59688657+Cork0158@users.noreply.github.com>
2026-03-06 23:22:12 +0900

    replace: astro

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+
+export default defineConfig({
+  site: "https://cork0158.github.io/",
+  // The project runs on GitHub Pages; `base` is automatically inferred for GitHub Pages
+  // but can be set explicitly if you deploy to a subpath.
+  // base: '/Cork0158.github.io/',
+});
